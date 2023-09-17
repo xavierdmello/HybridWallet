7045109537ba6196df3a2fcf6d6b64793607864f
Xavier DMello <xaviermdmello@gmail.com>
2023-09-16 23:34:23 -0400

    finish adding wagmi

--- src/main.tsx
+++ src/main.tsx
@@ -4,20 +4,28 @@ import App from './App.tsx'
 import './styles/index.css'
 import { ThemeProvider } from "@mui/material/styles";
 import { SafeThemeProvider } from "@safe-global/safe-react-components";
-import { configureChains } from "wagmi";
+import { configureChains , createConfig, WagmiConfig} from "wagmi";
 import {optimismGoerli} from "wagmi/chains"
 import { publicProvider } from "wagmi/providers/public";
 
 const { chains, publicClient, webSocketPublicClient } = configureChains([optimismGoerli], [publicProvider()]);
 
+const config = createConfig({
+  autoConnect: true,
+  publicClient,
+  webSocketPublicClient,
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <SafeThemeProvider mode="dark">
-      {(safeTheme) => (
-        <ThemeProvider theme={safeTheme}>
-          <App />
-        </ThemeProvider>
-      )}
-    </SafeThemeProvider>
+    <WagmiConfig config={config}>
+      <SafeThemeProvider mode="dark">
+        {(safeTheme) => (
+          <ThemeProvider theme={safeTheme}>
+            <App />
+          </ThemeProvider>
+        )}
+      </SafeThemeProvider>
+    </WagmiConfig>
   </React.StrictMode>
 );
