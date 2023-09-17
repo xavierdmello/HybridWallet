bc6878f4513b1a169165f12bfc3c2036c811932c
Xavier DMello <xaviermdmello@gmail.com>
2023-09-16 23:33:05 -0400

    add more packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './styles/index.css'
+import { ThemeProvider } from "@mui/material/styles";
+import { SafeThemeProvider } from "@safe-global/safe-react-components";
+import { configureChains } from "wagmi";
+import {optimismGoerli} from "wagmi/chains"
+import { publicProvider } from "wagmi/providers/public";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const { chains, publicClient, webSocketPublicClient } = configureChains([optimismGoerli], [publicProvider()]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <SafeThemeProvider mode="dark">
+      {(safeTheme) => (
+        <ThemeProvider theme={safeTheme}>
+          <App />
+        </ThemeProvider>
+      )}
+    </SafeThemeProvider>
+  </React.StrictMode>
+);
