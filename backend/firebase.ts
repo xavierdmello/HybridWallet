e8ab0e1d9090183260814b7e6488cd364e2d1e97
Xavier DMello <xaviermdmello@gmail.com>
2023-09-17 03:32:48 -0400

    bakcend updates

---
+++ backend/firebase.ts
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, onValue, ref } from "firebase/database";
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  databaseURL: "https://hybrid-wallet-263c3-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export default db;
--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,24 @@
-console.log("hello world")
+import { getDatabase, onValue, ref,onChildAdded } from "firebase/database";
+import { ethers } from "ethers";
+import db from "./firebase";
+
+async function main() {
+  const newSendboxMessageRef = ref(db, "sendbox/");
+  let initialDataLoaded = false;
+  onValue(newSendboxMessageRef, () => {
+    initialDataLoaded = true;
+  });
+  onChildAdded(newSendboxMessageRef, (snapshot) => {
+    if (initialDataLoaded == true) {
+      const emailHash = snapshot.key;
+      const accountData = snapshot.val();
+      const account = accountData.address;
+        
+    }
+  });
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
